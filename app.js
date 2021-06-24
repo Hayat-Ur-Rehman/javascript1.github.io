ece1f420aa8f9005bea8e01983ea82ba41b0e111
Hayat-Ur-Rehman <huzaifahayat386@gmail.com>
2021-06-24 23:55:15 +0500

    my JavaScript website
    Practice to JavaScript

---
+++ app.js
@@ -0,0 +1,132 @@
+// alert("Hello World");
+// console.log("Hello World");
+// document.write("Hello World");
+
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// alert(c);
+
+// var x = "ali";
+// var y = 10;
+// var z = a+b;
+// alert(c);
+
+
+// var a = 5;
+// var b = a++;
+// var c = a + b;          11 Ans
+// alert(c);
+
+// var a = 5;
+// var b = ++a;
+
+// alert(a + b);
+
+
+
+// var userName = prompt("Enter User Name");
+// var userAge = prompt("Enter User Age");
+
+// console.log (userName);
+// console.log (userAge);
+
+
+
+
+
+// var a = 5;
+// var b = 10;
+
+// var c = a++ + --a - ++a + --b - ++b;
+
+// alert(c)
+
+// var a = 5;
+// var b = 10;
+// var c = 15;
+
+// var d = (a+b) * c;
+
+// alert(d);
+
+
+
+/////**js work in class on 15-6-21**///////
+
+// var islamiat= +prompt("enter you islamiat marks");
+
+// var english= +prompt("enter you english marks");
+
+// var math= +prompt("enter you math marks");
+
+// var science= +prompt("enter you science marks");
+
+// var urdu= +prompt("enter you urdu marks");
+
+// var obt= islamiat+english+math+science+urdu
+// //alert(obt);
+// var total= 500;
+// var per= obt*100/total
+// alert(per)
+
+// if (per >= 79 && per < 90) {
+//     alert('your grade is A+')
+// }
+// else {
+//     alert('ap bhot laik hoo')
+// }
+
+
+
+
+//thapa video 5 if else statment  //
+// var marks = 100;
+// if (marks == 200) {
+//     document.write("Yes your marks is correct");
+// }else{
+//     document.write("Yes your marks is incorrect");
+// }
+
+//thapa video 2  var, let & const//
+
+// var whoWillWinToday = "Pakistan";
+
+// if(true){
+//     var whoWillWinToday = "NewZeland";
+//     console.log(whoWillWinToday);
+// }
+// console.log(whoWillWinToday);
+
+//let//
+// let whoWillWinToday = "Pakistan";
+
+//     if(true){
+//     let whoWillWinToday = "NewZeland";
+//     console.log(whoWillWinToday);
+//  }
+//  console.log(whoWillWinToday);
+
+
+ //const same result aye gaa//
+// const whoWillWinToday = "Pakistan";
+
+// if(true){
+// const whoWillWinToday = "NewZeland";
+// console.log(whoWillWinToday);
+// }
+// console.log(whoWillWinToday);
+
+
+
+
+
+
+
+
+
+
+
+
+
